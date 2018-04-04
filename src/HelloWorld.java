2f2deaf7c698fef4a21cb4230df6cd46cb65f567
snapmic <snapmic@gmail.com>
2018-04-04 18:51:34 +0200

    comments added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,11 +6,17 @@ public class HelloWorld {
 		System.out.println("My first program" + " Java no. 2");
 		System.out.println("My first program" + " Java no. 3");
 		System.out.println("My first program" + " Java no. 4");
-		/* the "println" function after finishing the
-		 *  task (for example text) starts the next text from the new line
+		/*
+		 * the "println" function after finishing the task (for example text) starts the
+		 * next text from the new line
 		 */
 		System.out.print("next program ");
-		System.out.print("next program"	);
+		System.out.print("next program");
+		/*
+		 * the "print" function, unlike the "println" function, does not create after
+		 * finishing its task (for example text) it does not make the transition to the
+		 * next line.
+		 */
 	}
 
 }
