3ff11f7cfdd6926a2c67fab2750aa95eceacdc35
snapmic <snapmic@gmail.com>
2018-04-04 18:37:10 +0200

    Pierwsze programy.

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("My first program" + " Java no. 1");
+		System.out.println("My first program" + " Java no. 2");
+		System.out.println("My first program" + " Java no. 3");
+		System.out.println("My first program" + " Java no. 4");
+		/* the "println" function after finishing the
+		 *  task (for example text) starts the next text from the new line
+		 */
+		System.out.print("next program ");
+		System.out.print("next program"	);
+	}
+
+}
---
+++ src/Pojazd.java
@@ -0,0 +1,19 @@
+
+public class Pojazd {
+	String czyJedzie() {
+		return "Pojazd jedzie";
+	}
+
+	String jakJedzie(String tekst) {
+		return tekst;
+	}
+
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Nie przykazano ¿adnych parametrów");
+		} else {
+			System.out.println("Przykazano " + args.length + "parametrów");
+		}
+	}
+
+}
