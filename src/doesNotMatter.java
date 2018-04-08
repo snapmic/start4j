b1acd0a849827267d9422d9b46362b07461fa87f
snapmic <snapmic@gmail.com>
2018-04-08 18:58:58 +0200

    added program "Operatory"

--- src/doesNotMatter.java
+++ src/doesNotMatter.java
@@ -1,9 +1,25 @@
+import java.awt.Button;
 
 public class doesNotMatter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String s;
+		Button b;
+		b = new Button();
+		s = new String("Tekstu");
+		String lancuch = "Przykladowy tekst";
+		int[] tablicaInt;
+		tablicaInt = new int [10];
+		tablicaInt[0] = 5;
+		tablicaInt[1] = 15;
+		int[] nowaTablica = {1, 3, 6, 9,};
+		String[] imiona = new String [5];
+		imiona[0] = "Jan";
+		imiona[1] = "Franek";
+		imiona[2] = "Mateusz";
+		imiona[3] = "Micha³";
+		imiona[4] = "Zosia";
+		System.out.println(imiona[2]);
 	}
 
 }
